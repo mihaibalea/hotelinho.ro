e38ecfd03bc9d34adc29f348f9b2662c8283bb0a
Mihai <balea.mihai93@gmail.com>
2015-08-04 10:37:41 +0300

    Connected the app to DB

--- src/main/java/controller/HotelsController.java
+++ src/main/java/controller/HotelsController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import business.Business;
+import dao.model.HotelDTO;
 
 @RestController
 @RequestMapping("/hotels")
@@ -13,10 +17,9 @@ public class HotelsController {
 	@Autowired
 	private Business business;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String getAllHotels() {
-		System.out.println("This print is from getAllHotels");
-		return " ";
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<HotelDTO> getAllHotels() {
+		return business.getAllCourses();
 	}
 
 }
